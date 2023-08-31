8fc15ef6e20f31948a81f723f8ff2dce087025ad
Tej Bari <136843098+tejbari5@users.noreply.github.com>
2023-08-31 19:56:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,26 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+function downloadImage(image) {
+            return new Promise((resolve, reject) => {
+                const imgElement = new Image();
+                imgElement.onload = () => resolve(imgElement);
+                imgElement.onerror = () => reject(new Error(`Failed to load image's URL: ${image.url}`));
+                imgElement.src = image.url;
+            });
+        }
+
+btn.addEventListener('click', async () => {
+            
+            output.innerHTML = ''; // Clear previous content
+
+            try {
+                const downloadedImages = await Promise.all(images.map(downloadImage));
+                downloadedImages.forEach(imgElement => {
+                    output.appendChild(imgElement);
+                });
+            } catch (error) {
+                console.error(error);
+            }
+        });
